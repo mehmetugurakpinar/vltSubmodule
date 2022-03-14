9d8a49af372c63d963207863ba1454ae14e51edf
mehmetugurakpinar <60554065+mehmetugurakpinar@users.noreply.github.com>
2022-03-14 19:41:49 +0300

    Initial

---
+++ src/test/java/com/example/trySubmodule/TrySubmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trySubmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrySubmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
